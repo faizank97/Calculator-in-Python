0bbe497f9c925af4d88a8854e4138738093b9b48
Faizan Khalid <85803774+faizank97@users.noreply.github.com>
2022-10-08 15:56:52 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter number one: "))
+num2 = float(input("Enter number two: "))
+op = input("Enter operator")
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+
